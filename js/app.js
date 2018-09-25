8824f2bd3c8b9c0b0d48dccecc5a0dc0f1eeea90
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2018-09-24 21:44:37 -0300

    Check if opened cards match

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ function shuffle(array) {
  * set up the event listener for a card. If a card is clicked:
  *  [x] display the card's symbol (put this functionality in another function that you call from this one)
  *  [x] add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  [ ] if the list already has another card, check to see if the two cards match
+ *  [x] if the list already has another card, check to see if the two cards match
  *    [ ] if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    [ ] if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    [ ] increment the move counter and display it on the page (put this functionality in another function that you call from this one)
@@ -43,9 +43,13 @@ let toggledCards = [];
 /* set up the event listener for a card. */
 deck.addEventListener('click', event => {
     const clickTarget = event.target;
-    if (clickTarget.classList.contains('card')) {
+    if (clickTarget.classList.contains('card') && toggledCards.length < 2 ) {
         toggleCard(clickTarget);
         addToggleCard(clickTarget);
+
+        if (toggledCards.length === 2) {
+            checkForMatch(toggledCards[0], toggledCards[1]);
+        }
     }
 });
 
@@ -59,4 +63,18 @@ function toggleCard(card) {
 function addToggleCard(card) {
     toggledCards.push(card);
     console.log(toggleCard);
+}
+
+/* Get the "suit" of a card */
+function getSuit(card) {
+    return card.firstElementChild.className;
+}
+
+/* Check for a match */
+function checkForMatch(card1, card2) {
+    if (getSuit(card1) === getSuit(card2)) {
+        console.log('The cards do match!');
+    } else {
+        console.log("The cards doesn't match!");
+    }
 }
